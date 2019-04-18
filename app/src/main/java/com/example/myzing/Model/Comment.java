e87d2c3d55436d6f3168f83e79d488aa76c5c5df
THAO HAP <huongthaonb24@gmail.com>
2019-04-18 23:49:42 +0700

    add comment

--- app/src/main/java/com/example/myzing/Model/Comment.java
+++ app/src/main/java/com/example/myzing/Model/Comment.java
@@ -1,5 +1,47 @@
 package com.example.myzing.Model;
 
-public class Comment {
+import java.util.Date;
 
+public class Comment {
+    private int id;
+    private String contentComent;
+    private Date timeComment;
+
+    public Comment() {
+    }
+
+    public Comment(int id, String contentComent, Date timeComment) {
+        this.id = id;
+        this.contentComent = contentComent;
+        this.timeComment = timeComment;
+    }
+
+    public Comment(String contentComent, Date timeComment) {
+        this.contentComent = contentComent;
+        this.timeComment = timeComment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContentComent() {
+        return contentComent;
+    }
+
+    public void setContentComent(String contentComent) {
+        this.contentComent = contentComent;
+    }
+
+    public Date getTimeComment() {
+        return timeComment;
+    }
+
+    public void setTimeComment(Date timeComment) {
+        this.timeComment = timeComment;
+    }
 }
