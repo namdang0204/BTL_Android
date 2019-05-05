42da3200cf0c6b7f094a1179957033395e09223e
ThaoHap <huongthaonb24@gmail.com>
2019-05-05 14:44:12 +0700

    Topic & Genre

---
+++ app/src/main/java/com/example/myzing/Activity/DanhSachTopicGenreActivity.java
@@ -0,0 +1,15 @@
+package com.example.myzing.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myzing.R;
+
+public class DanhSachTopicGenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach_topic_genre);
+    }
+}
--- app/src/main/java/com/example/myzing/Fragment/Fragment_Topic_Genre.java
+++ app/src/main/java/com/example/myzing/Fragment/Fragment_Topic_Genre.java
@@ -1,5 +1,6 @@
 package com.example.myzing.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,11 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.myzing.Activity.DanhSachTopicGenreActivity;
+import com.example.myzing.Model.TopicGenre;
 import com.example.myzing.R;
+import com.example.myzing.Service.APIService;
+import com.example.myzing.Service.DataService;
+
+import java.util.List;
 
 public class Fragment_Topic_Genre extends Fragment {
     View view;
-    TextView tv_topic_genre, tv_viewmore_topic_genre;
+    TextView tvtitle_topic_genre, tv_viewmore_topic_genre;
     RecyclerView recyclerView_Topic_Genre;
 
 
@@ -22,15 +29,24 @@ public class Fragment_Topic_Genre extends Fragment {
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         view= inflater.inflate(R.layout.fragment_topic_genre,container,false);
-        tv_topic_genre= view.findViewById(R.id.txtview_topic_genre);
+        tvtitle_topic_genre= view.findViewById(R.id.txtview_title_topic_genre);
         tv_viewmore_topic_genre= view.findViewById(R.id.txtview_viewmore_topic_genre);
         recyclerView_Topic_Genre= view.findViewById(R.id.recycleview_topic_genre);
+        getData();
         tv_viewmore_topic_genre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent=new Intent(getActivity(), DanhSachTopicGenreActivity.class);
+                startActivity(intent);
             }
         });
         return view;
     }
+
+    private void getData() {
+        DataService dataService= APIService.getDataService();
+        List<TopicGenre>
+    }
+
+
 }
