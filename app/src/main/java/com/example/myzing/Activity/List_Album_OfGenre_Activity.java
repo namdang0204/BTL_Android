39ba53495b3ca6c08e4b30f7d14e44b770419749
ThaoHap <huongthaonb24@gmail.com>
2019-05-07 16:41:41 +0700

    Click Genre

--- app/src/main/java/com/example/myzing/Activity/List_Album_OfGenre_Activity.java
+++ app/src/main/java/com/example/myzing/Activity/List_Album_OfGenre_Activity.java
@@ -3,25 +3,33 @@ package com.example.myzing.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.myzing.Adapter.ListAlbumOfGenreAdapter;
+import com.example.myzing.Model.Album;
 import com.example.myzing.Model.Genre;
 import com.example.myzing.Model.Topic;
 import com.example.myzing.R;
 import com.example.myzing.Service.APIService;
 import com.example.myzing.Service.DataService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class List_Album_OfGenre_Activity extends AppCompatActivity {
     Genre genre;
-    Toolbar  toolbar_List_Album__OfGenre;
+    Toolbar toolbar_List_Album__OfGenre;
     RecyclerView recyclerView_List_Album__OfGenre;
+    ListAlbumOfGenreAdapter listAlbumOfGenreAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +41,29 @@ public class List_Album_OfGenre_Activity extends AppCompatActivity {
     }
 
     private void GetData() {
-        DataService dataService= APIService.getDataService();
+        DataService dataService = APIService.getDataService();
+        Call<List<Album>> listCall = dataService.GetListAlbumOfGenre(genre.getIdGenre());
+        listCall.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                ArrayList<Album> arrayAlbum = (ArrayList<Album>) response.body();
+//                Log.d("aaa",arrayAlbum.size()+"");
+                listAlbumOfGenreAdapter = new ListAlbumOfGenreAdapter(List_Album_OfGenre_Activity.this, arrayAlbum);
+
+                recyclerView_List_Album__OfGenre.setLayoutManager(new GridLayoutManager(List_Album_OfGenre_Activity.this, 2));
+                recyclerView_List_Album__OfGenre.setAdapter(listAlbumOfGenreAdapter);
+            }
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+
+            }
+        });
 
     }
 
     private void init() {
-        toolbar_List_Album__OfGenre= findViewById(R.id.toolbar_list_album_ofgenre);
-        recyclerView_List_Album__OfGenre=findViewById(R.id.recycleview_list_album_ofgenre);
+        toolbar_List_Album__OfGenre = findViewById(R.id.toolbar_list_album_ofgenre);
+        recyclerView_List_Album__OfGenre = findViewById(R.id.recycleview_list_album_ofgenre);
         setSupportActionBar(toolbar_List_Album__OfGenre);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(genre.getNameGenre());
@@ -53,10 +77,10 @@ public class List_Album_OfGenre_Activity extends AppCompatActivity {
     }
 
     private void GetIntent() {
-        Intent intent= getIntent();
-        if(intent.hasExtra("genre")){
-            genre= (Genre) intent.getSerializableExtra("genre");
-//            Toast.makeText(this,genre.getNameGenre(), Toast.LENGTH_SHORT).show();
+        Intent intent = getIntent();
+        if (intent.hasExtra("genre")) {
+            genre = (Genre) intent.getSerializableExtra("genre");
+            Toast.makeText(this,genre.getNameGenre(), Toast.LENGTH_SHORT).show();
         }
     }
 
---
+++ app/src/main/java/com/example/myzing/Adapter/ListAlbumOfGenreAdapter.java
@@ -0,0 +1,61 @@
+package com.example.myzing.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.myzing.Model.Album;
+import com.example.myzing.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ListAlbumOfGenreAdapter extends RecyclerView.Adapter<ListAlbumOfGenreAdapter.ViewHolder> {
+    Context context;
+    ArrayList<Album> arrayAlbum;
+
+    public ListAlbumOfGenreAdapter(Context context, ArrayList<Album> arrayAlbum) {
+        this.context = context;
+        this.arrayAlbum = arrayAlbum;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater  inflater= LayoutInflater.from(context);
+        View view= inflater.inflate(R.layout.dong_album_ofgenre, viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Album album= arrayAlbum.get(i);
+        Picasso.with(context).load(album.getImageAlbum()).into(viewHolder.imageView_List_Album_OfGenre);
+        viewHolder.textView_List_Album_OfGenre.setText(album.getNameAlbum());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayAlbum.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView_List_Album_OfGenre;
+        TextView textView_List_Album_OfGenre;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView_List_Album_OfGenre= itemView.findViewById(R.id.imageview_image_album_ofgenre);
+            textView_List_Album_OfGenre= itemView.findViewById(R.id.textview_name_album_ofgenre);
+        }
+    }
+
+
+
+}
+
+
